a9a9ffa2eec29c4122dc0cff4229850146453740
kn-kn <xkeitax0709@gmail.com>
2021-04-14 10:26:17 +0900

    非同期課題

--- lib/async/async_screen.dart
+++ lib/async/async_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:youtube_demo/prefs.dart';
+import 'package:youtube_demo/async/data_validation.dart';
 
 //画面初期化時に、Preferencesより名前、年齢、誕生日をロードし画面に表示する
 //  - 未保存の場合、すべての項目に未設定と表示する
@@ -16,7 +17,7 @@ import 'package:youtube_demo/prefs.dart';
 //ダイアログを閉じると保存した値が更新されていること
 // ■確認項目
 // StatefulWidget, async, await, Future
-// Dialog, TextEdittingController, Validation
+// Dialog, TextEditingController, Validation
 
 class AsyncScreen extends StatefulWidget {
   @override
@@ -24,21 +25,49 @@ class AsyncScreen extends StatefulWidget {
 }
 
 class _AsyncScreenState extends State<AsyncScreen> {
+  // 変更可能なフィールド
   String _name;
   int _age;
   String _birthday;
+
+  // TextEditingControllerを格納する為の変数
+  // TextFieldの入力内容を管理し、入力内容を画面に描画できる
+  var _nameController;
+  var _ageController;
+  var _birthdayController;
+
+  // アプリ起動時に一度だけ実行する処理 super.initState()は、必須
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  _loadUserData() async {
+    _name = await Prefs.getName();
+    _age = await Prefs.getAge();
+    _birthday = await Prefs.getBirthDay();
+    setState(() {});
+  }
+
+  _saveUserData(String name, String age, String birthday) async {
+    await Prefs.setName(name);
+    await Prefs.setAge(int.parse(age));
+    await Prefs.setBirthDay(birthday);
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: _createBody(),
+        child: _createBody(context),
       ),
       floatingActionButton: _createFloatingActionButton(),
     );
   }
 
-  Widget _createBody() {
+  Widget _createBody(context) {
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
@@ -79,14 +108,136 @@ class _AsyncScreenState extends State<AsyncScreen> {
       backgroundColor: Colors.grey.shade200,
       child: Icon(Icons.edit, color: Colors.blue.shade300),
       onPressed: () {
-        // _createDialog();
+        showDialog(
+          context: context,
+          builder: (_) => _alertDialog(context),
+        ).then((value) => _loadUserData());
       },
     );
   }
 
-  // Widget _createDialog() {
-  //   return showDialog(BuildContext context) {
-  //
-  //   }
-  // }
+  Widget _alertDialog(BuildContext context) {
+    _nameController = TextEditingController(text: _name);
+    _ageController = TextEditingController(text: _age.toString());
+    _birthdayController = TextEditingController(text: _birthday);
+
+    return AlertDialog(
+      content: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            controller: _nameController,
+            decoration: const InputDecoration(
+              hintText: '名前を入力して下さい',
+              labelText: '名前',
+            ),
+            onChanged: (String text) {
+              setState(() {
+                _name = text;
+              });
+            },
+          ),
+          TextField(
+            controller: _ageController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              hintText: '年齢を入力',
+              labelText: '年齢',
+            ),
+            onChanged: (String num) {
+              setState(() {
+                _age = int.parse(num);
+              });
+            },
+          ),
+          TextField(
+            controller: _birthdayController,
+            decoration: const InputDecoration(
+              hintText: '誕生日を入力',
+              labelText: '誕生日',
+            ),
+            onChanged: (String text) {
+              setState(() {
+                _birthday = text;
+              });
+            },
+          ),
+        ],
+      ),
+      actions: [
+        ElevatedButton(
+          child: Text('キャンセル'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        ElevatedButton(
+          child: Text('保存'),
+          onPressed: () {
+            if (_isValid()) {
+              _saveUserData(_nameController.text, _ageController.text,
+                  _birthdayController.text);
+              setState(() {
+                Navigator.pop(context);
+              });
+            }
+          },
+        ),
+      ],
+    );
+  }
+
+  // バリデーションメソッド
+  bool _isValid() {
+    final name = _nameController.text;
+    final age = _ageController.text;
+    final birthday = _birthdayController.text;
+
+    List<String> errorMessage = <String>[];
+
+    // 名前が空のとき
+    if (name.isEmpty) {
+      errorMessage.add('名前が空です');
+    }
+
+    //年齢が空または、数値(数値形式)でない時
+    if (age.isEmpty || double.tryParse(name) != null) {
+      errorMessage.add('年齢が空もしくは数字ではありません');
+    }
+
+    //誕生のフォーマットじゃない時
+    if (!DateValidation.isDate(birthday)) {
+      errorMessage.add('誕生日の形式が正しくありません');
+    }
+
+    if (errorMessage.isNotEmpty) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          final List<Widget> children = [];
+          errorMessage.forEach((element) {
+            children.add(Text(element));
+          });
+          return CupertinoAlertDialog(
+            title: Text("入力エラー"),
+            content: Padding(
+              padding: const EdgeInsets.only(top: 4.0, bottom: 4.0),
+              child: Column(
+                children: children,
+              ),
+            ),
+            actions: <Widget>[
+              CupertinoDialogAction(
+                child: Text("閉じる"),
+                isDestructiveAction: true,
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          );
+        },
+      );
+      return false;
+    }
+    return true;
+  }
 }
