aab2ff8d251061b243d623a5036f96855e7efe82
gitknkn <atio.asu@gmail.com>
2021-04-19 07:17:17 +0900

    qiita-client 完成

---
+++ lib/mvvm/screen/home/home_screen_notifier.dart
@@ -0,0 +1,36 @@
+import 'package:youtube_demo/mvvm/repository/qiita_repository.dart';
+import 'package:youtube_demo/mvvm/state/home_state.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeStateNotifier extends StateNotifier<HomeState> {
+  QiitaRepository _repository = QiitaRepository();
+
+  HomeStateNotifier() : super(HomeState());
+
+  fetchQiitaItems(String tag) async {
+    state = state.copyWith(
+      isLoading: true,
+    );
+
+    final qiitaItems = await _repository.fetchQiitaItems(tag);
+
+    if (qiitaItems.length != 0) {
+      state = state.copyWith(
+          isLoading: false,
+          isReadyData: true,
+          currentTag: tag,
+          qiitaItems: qiitaItems);
+    } else {
+      state = state.copyWith(
+          isLoading: false, isReadyData: false, qiitaItems: qiitaItems);
+    }
+  }
+
+  onBackHome() {
+    state = state.copyWith(
+      isLoading: false,
+      isReadyData: false,
+      currentTag: "",
+    );
+  }
+}
